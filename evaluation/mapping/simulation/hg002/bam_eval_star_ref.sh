8c1f0101dda9bd4639a5dbdafa4b5b693fcce638
jonassibbesen <j.a.sibbesen@gmail.com>
2023-01-25 14:46:28 +0100

    add README guides to evaluation folder

--- evaluation/mapping/simulation/hg002/bam_eval_star_ref.sh
+++ evaluation/mapping/simulation/hg002/bam_eval_star_ref.sh
@@ -6,4 +6,4 @@ samtools sort --threads 4 -n -O BAM sim_mc_grch38_hg002_vgrna_k32_h${HAP_IDX}.st
 
 samtools flagstat --threads 4 sim_mc_grch38_hg002_vgrna_k32_h${HAP_IDX}.star_fixed_rsort.bam
 
-calc_vg_benchmark_stats sim_mc_grch38_hg002_vgrna_k32_h${HAP_IDX}.star_fixed_rsort.bam benchmark_hsts.reheader.bam sim_mc_grch38_hg002_vgrna_k32_h${HAP_IDX}.read_origin.txt 3 null null null null > bam_eval_r1_star_sim_hg002_gen38_h${HAP_IDX}_ovl3.txt
+calc_vg_benchmark_stats sim_mc_grch38_hg002_vgrna_k32_h${HAP_IDX}.star_fixed_rsort.bam hg002_hsts_grch38.bam sim_mc_grch38_hg002_vgrna_k32_h${HAP_IDX}.read_origin.txt 3 null null null null > bam_eval_r1_star_sim_hg002_gen38_h${HAP_IDX}_ovl3.txt
